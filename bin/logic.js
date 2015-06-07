b6feef7180d08dc4b0a8ae53402cf1f2839a9c48
mbusenitz <mbusenitz@gmail.com>
2015-06-07 04:52:13 -0700

    Squashed 'test/plank/' changes from a4e06fa..29c4e98
    29c4e98 cleanup
    1b4cea7 working
    c75b705 build attemp
    f254ad0 build attemp
    2a6be7d generate'
    
    git-subtree-dir: test/plank
    git-subtree-split: 29c4e98fe9fed96107408547083db1d735bcaac8

--- bin/logic.js
+++ bin/logic.js
@@ -5,77 +5,76 @@ var assert = require( 'assert' )
 
 function Logic(base) {
 
-	this.traverse = function(testDir) {
-		return new Promise(function(resolve, reject) {
-				console.log( 'got traverse' );
-				fs.exists(testDir, function(exists) {
-					console.log( 'got exists' );
-					if (exists) { 
-						resolve( testDir ); 
-					}
-					else {
-						Printer.cursor.red();
-						process.stdout.write( 'invalid test definition path: ');
-						Printer.cursor.reset();
-						console.log( testDir ); 
-						reject();
-					}
-				});
-			});
+  this.traverse = function(o) {
+    return new Promise(function(resolve, reject) {
+        fs.exists( o.testDir, function(exists) {
+          if (exists) { 
+            resolve( o ); 
+          }
+          else {
+            Printer.cursor.red();
+            process.stdout.write( 'invalid test definition path: ');
+            Printer.cursor.reset();
+            console.log( o.testDir ); 
+            reject();
+          }
+        });
+      });
 
-	};
+  };
 
-	this.generate = function(defFile, testDir) {
-		return new Promise(function(resolve, reject) {
-				Printer.begin( defFile, 'generate' );
-				base.generate( defFile, testDir, function( exitCode, buildDir ){
-					if (!exitCode) {
-						Printer.finishGreen( defFile, 'generate' );
-						resolve(buildDir)
-					}
-					else {
-						Printer.finishRed( defFile ) ; 
-						reject(buildDir); 
-					}
-				});
-			});
-	};
+  this.generate = function(o) {
+    return new Promise(function(resolve, reject) {
+      Printer.begin( o.defFile, 'generate' );
+      base.generate( o, 
+        function( exitCode, buildDir){
+          o['buildDir'] = buildDir;
+          if (!exitCode) {
+            Printer.finishGreen( o.defFile, 'generate' );
+            resolve(o);
+          }
+          else {
+            Printer.finishRed( o.defFile ) ; 
+            reject(o); 
+          }
+        });
+      });
+  };
 
-	this.run = function(o) {
-		console.log( "outside logic run" );
-		return new Promise(function(resolve, reject) {
-				console.log( "logic run" );
-				Printer.begin( defFile, 'run' );
-				base.run( o, function(exitCode) {
-					if (!exitCode) {
-						Printer.finishGreen( o.defFile );
-						console.log( '=> ' + o.targetName + ' passed' );
-						resolve(exitCode);
-					}
-					else {
-						Printer.finishRed( o.defFile ) ; 
-						console.log( '=> ' + o.targetName + ' failed with exit code:', exitCode );
-						reject(exitCode);
-					}
-				});
-			});
-	}; 
+  this.build = function(o) {
+    return new Promise( function(resolve, reject) {
+      Printer.begin( o.defFile, 'build' );
+      base.build( o, function( o ) { 
+        if (!o.exitCode) {
+          Printer.finishGreen( o.defFile );
+          resolve( o );
+        }
+        else {
+          Printer.finishRed( o.defFile );
+          reject(o); 
+        }
+      });
+    });
+  };
 
-	this.build = function(defFile, testDir) {
-		return new Promise( function(resolve, reject) {
-				Printer.begin( defFile, 'build' );
-				base.build( defFile, testDir, function( exitCode, targetName, buildDir ) { 
-					if (!exitCode) {
-						Printer.finishGreen( defFile );
-						resolve( { defFile: defFile, buildDir: buildDir, targetName: targetName } );
-					}
-					else {
-						Printer.finishRed( defFile );
-						reject(defFile); 
-					}
-				});
-			});
-	};
+  this.run = function(o) {
+    return new Promise(function(resolve, reject) {
+      Printer.begin( o.defFile, 'run' );
+      base.run( o, function(exitCode) {
+        if (!exitCode) {
+          Printer.finishGreen( o.defFile );
+          console.log( '=> ' + o.targetName + ' passed' );
+          o['exitCode'] = exitCode;
+          resolve(o);
+        }
+        else {
+          Printer.finishRed( o.defFile ) ; 
+          console.log( '=> ' + o.targetName + ' failed with exit code:', exitCode );
+          reject(o);
+        }
+      });
+    });
+  }; 
 };
 
 module.exports = Logic;
