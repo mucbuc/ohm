81cc2a4373cb92a07ba7acbab65ebabceb826dc0
Ubuntu <ubuntu@ip-172-31-16-51.us-west-2.compute.internal>
2019-12-21 06:40:24 +0000

    emit with args

--- test/src/emit_with_args.cpp
+++ test/src/emit_with_args.cpp
@@ -1,50 +1,18 @@
-
-#include <iostream>
-#include <lib/ohm/src/quemitter.h>
 #include <tmp/src/test.h>
-#include <vector>
 
-using namespace std;
-using namespace om636;
-
-template <template <class, class> class T>
-void check_emit_with_args()
-{
-    typedef T<string, function<void(int, int)>> emitter_type;
-    typedef typename emitter_type::listener_type listener_type;
-    emitter_type e;
-
-    unsigned test_passed(0);
-    string event("e");
-
-    listener_type p(e.once(event, [&](int i, int j) {
-        ASSERT(i == 99);
-        ASSERT(j == 3);
-        ++test_passed;
-    }));
-
-    listener_type q(e.on(event, [&](int i, int j) {
-        ASSERT(i == 99);
-        ASSERT(j == 3);
-        ++test_passed;
-    }));
-
-    e.emit(event, 99, 3);
-
-    ASSERT(test_passed == 2);
-
-    FOOTER;
-}
-
-template <class T, class U>
-using QueuedEmitter = om636::control::Quemitter<T, U>;
+#include <lib/ohm/src/emitter_impl.h>
 
 int main()
 {
-    using namespace std;
+    unsigned sum(0);
+    auto f = [&](unsigned i, unsigned j) { sum += i * j; };
 
-    check_emit_with_args<om636::control::Emitter>();
-    check_emit_with_args<QueuedEmitter>();
+    om636::control::EmitterImpl<std::string, unsigned, unsigned> e;
+    auto p(e.once("e", f));
+    auto q(e.on("e", f));
+    e.interupt("e", 7, 3);
+
+    ASSERT(sum == 42 && "emit with args passed");
 
     return 0;
 }
