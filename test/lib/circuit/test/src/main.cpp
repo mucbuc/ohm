9877e777b93a54d396248ad449f6ba5abf324733
mucbuc <mbusenitz@gmail.com>
2020-01-16 20:22:05 -0800

    circuit

---
+++ test/lib/circuit/test/src/main.cpp
@@ -0,0 +1,33 @@
+#include <memory>
+#include <vector>
+
+#include <tmp/src/test.h>
+
+#include <lib/circuit/src/index.h>
+
+template <class T>
+void test_wait_pop(T s)
+{
+    auto tmp = std::thread([=]() {
+        int i(0);
+        s->wait_pop(i);
+        ASSERT(i == 99);
+    });
+
+    s->push(99);
+    tmp.join();
+}
+
+int main()
+{
+    using namespace om636::twice_size;
+
+    test_wait_pop(std::make_shared<CircuitStack<int>>());
+    test_wait_pop(std::make_shared<CircuitQueue<int>>());
+
+
+    CircuitQueue<int> a, b;
+    a.swap(b);
+
+    return 0;
+}
