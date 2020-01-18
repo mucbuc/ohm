71aba1ef472aa23c9c9a8529b5258599a6289c16
mucbuc <mbusenitz@gmail.com>
2020-01-18 13:41:03 -0800

    fix emit once

--- test/src/emit_once.cpp
+++ test/src/emit_once.cpp
@@ -1,17 +1,15 @@
 #include <tmp/src/test.h>
 
 #include <iostream>
-#include <lib/ohm/src/emitter.h>
-#include <lib/ohm/src/quemitter.h>
-#include <vector>
+#include <lib/ohm/src/emitter_impl.h>
 
 using namespace std;
 using namespace om636;
+using namespace control;
 
-template <template <class, class> class T>
-void check_emit_once()
+int main()
 {
-    typedef T<string, function<void(int)>> emitter_type;
+    typedef EmitterImpl<string, int> emitter_type;
     emitter_type e;
 
     unsigned counter(0);
@@ -24,25 +22,12 @@ void check_emit_once()
         ++counter;
     }));
 
-    e.emit("hello", 0);
-    e.emit("hello", 0);
-    e.emit("allo", 9);
-    e.emit("allo", 9);
+    e.interupt("hello", 0);
+    e.interupt("hello", 0);
+    e.interupt("allo", 9);
+    e.interupt("allo", 9);
 
-    ASSERT(counter == 2);
-
-    FOOTER;
-}
-
-template <class T, class U>
-using QueuedEmitter = om636::control::Quemitter<T, U>;
-
-int main()
-{
-    using namespace std;
-
-    check_emit_once<om636::control::Emitter>();
-    check_emit_once<QueuedEmitter>();
+    ASSERT(counter == 2 && "emit once");
 
     return 0;
 }
