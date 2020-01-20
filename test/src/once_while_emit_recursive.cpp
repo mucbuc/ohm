9aae5bf8c8a46439e7b350c4166c1be22c6028bf
mucbuc <mbusenitz@gmail.com>
2020-01-20 12:08:50 -0800

    once tests

---
+++ test/src/once_while_emit_recursive.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <vector>
+
+#include <tmp/src/test.h>
+
+#include <lib/ohm/src/emitter_impl.h>
+
+int main()
+{
+    using namespace std;
+    using namespace om636;
+    using namespace control;
+
+    typedef EmitterImpl<string> emitter_type;
+    typedef typename emitter_type::listener_type listener_type;
+
+    emitter_type emitter;
+    vector<listener_type> listeners;
+
+    unsigned counter(0);
+
+    listeners.push_back(emitter.once("on", [&]() {
+        ++counter;
+        emitter.interupt("on");
+    }));
+
+    emitter.interupt("on");
+
+    ASSERT(counter == 1 && "check_once_while_emit_recursive");
+    return 0;
+}
