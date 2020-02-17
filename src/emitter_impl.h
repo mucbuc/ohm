8ff53cef8d7573b1a846d699614edacd09e68889
mucbuc <mbusenitz@gmail.com>
2020-02-16 16:59:36 -0800

    update dynamo/circuit

--- src/emitter_impl.h
+++ src/emitter_impl.h
@@ -6,8 +6,7 @@
 #include <set>
 
 #include "interface.h"
-#include <lib/dynamo/src/impl/batch.h>
-
+#include <lib/circuit/src/impl/queue.h>
 #include <lib/dynamo/src/impl/batch.h>
 
 namespace om636 {
@@ -25,7 +24,7 @@ namespace control {
         void interupt(event_type, U...) override;
 
     private:
-        typedef BatchImpl<U...> batch_type;
+        typedef BatchImpl<om636::circuit::QueuePolicy, U...> batch_type;
         typedef std::map<event_type, batch_type> map_type;
 
         map_type m_repeat;
