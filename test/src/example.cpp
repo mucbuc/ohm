3fc9415f7897964c3a996717877e4b09b5ed8404
Ubuntu <ubuntu@ip-172-31-16-51.us-west-2.compute.internal>
2020-03-28 11:08:32 +0000

    break build with example

--- test/src/example.cpp
+++ test/src/example.cpp
@@ -1,9 +1,13 @@
-#include <tmp/src/test.h>
-#include <lib/ohm/src/interface.h>
-
 #include <iostream>
 
+#include <tmp/src/test.h>
+
+#include <lib/ohm/src/emitter_impl.h>
+
 int main() {
-  std::cout << "hello" << std::endl;
+  using namespace om636::control;
+  using namespace std; 
+
+  auto e = make_Emitter<string, int>(); 
   return 0;
 }
