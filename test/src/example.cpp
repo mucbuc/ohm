089829ef81a8ad7a44851081c6bd62f7b19ea3e9
Ubuntu <ubuntu@ip-172-31-16-51.us-west-2.compute.internal>
2020-03-30 04:09:17 +0000

    disable example test

--- test/src/example.cpp
+++ test/src/example.cpp
@@ -2,12 +2,12 @@
 
 #include <tmp/src/test.h>
 
-#include <lib/ohm/src/emitter_impl.h>
+//#include <lib/ohm/src/emitter_impl.h>
 
 int main() {
-  using namespace om636::control;
+  //using namespace om636::control;
   using namespace std; 
 
-  auto e = make_Emitter<string, int>(); 
+ // auto e = make_Emitter<string, int>(); 
   return 0;
 }
