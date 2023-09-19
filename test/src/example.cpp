6e18d33ac4042aa1f6b2b0be43716a4e6f9e759d
mucbuc <mbusenitz@gmail.com>
2023-09-18 21:15:23 -0700

    example -> examples

--- test/src/example.cpp
+++ test/src/example.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-
 #include <tmp/src/test.h>
-
 #include <lib/ohm/src/index.h>
 
 int main()
@@ -10,8 +8,8 @@ int main()
     using namespace std;
 
     auto e = make_emitter<string, int>();
-    auto l = e->on("run", [](int) {
-        std::cout << "run" << std::endl;
+    auto l = e->on("run", [](int v) {
+        cout << "run " << v << endl;
     });
     e->interupt("run", 4);
     return 0;
